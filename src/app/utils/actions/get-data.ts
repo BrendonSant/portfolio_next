02dc56a98cece96d852220e94cc1ffc9e8144f4b
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2024-09-06 17:35:12 -0300

    inicio de rota para api

---
+++ src/app/utils/actions/get-data.ts
@@ -0,0 +1,16 @@
+export async function getDataHome() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/66db51289bf0dbeae28b1b85?read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`);
+
+      
+
+        if(!res.ok){
+            throw new Error("Failed to fetch data");
+        }
+
+        return res.json();
+    } catch(err){
+        throw new Error("Failed to fetch data");
+ 
+    }
+}
