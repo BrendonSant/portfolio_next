083526a626d035c8f025b2af070be8bd27a4ac01
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2024-09-06 11:21:23 -0300

    header/nav mais produção de submenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-col w-full min-h-96 h-screen bg-mcolor-navy-navy-700 mb-5">
-        <div className="bg-white">
-          <h1>teste</h1>
+        <div className=" mt-24">
+          <h1 className="text-2xl text-white">teste</h1>
         </div>
       </div>
       <div className="bg-slate-600 w-full h-96">
