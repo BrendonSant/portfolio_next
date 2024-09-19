1a9309125219e0645ff0df1ee96bfc33794f2478
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2024-09-18 21:27:39 -0300

    Ajuste em skills

--- src/app/components/home/about/index.tsx
+++ src/app/components/home/about/index.tsx
@@ -10,7 +10,7 @@ export function About({txtabout, url}: AboutProps){
     return(
         <section className="w-full h-80 flex flex-col justify-center items-center">
             <div className="w-full max-w-screen-2xl flex flex-col justify-center items-center">
-                <h2 className="text-4xl mb-6">ABOUT</h2>
+                <h2 className="text-5xl tracking-widest mb-36 ml2 font-bold">ABOUT</h2>
                 <div className="flex justify-around items-center w-full mt-8 ">
                     <Image  alt='Imagem About'
                     src={url}
--- src/app/components/home/skills/index.tsx
+++ src/app/components/home/skills/index.tsx
@@ -2,105 +2,81 @@
 
 import { HomeProps } from "@/app/utils/home.type";
 import Image from "next/image";
+import { useRef } from "react";
 
 
+export function Skills({ object }: HomeProps) {
+  const textRef = useRef<HTMLHeadingElement>(null);
 
-export function Skills({object}: HomeProps){
-  return(
-    <div className="flex w-full mt-52 justify-center  ">
-      <section  className="flex flex-col justify-center items-center max-w-screen-2xl w-full px-10">
-        <h2 className="text-4xl mb-36 ">SKILLS</h2>
+  
+
+  return (
+    <div className="flex w-full mt-52 justify-center">
+      <section className="flex flex-col justify-center items-center max-w-screen-2xl w-full px-10">
+        <h2 className=" ml2 text-5xl tracking-widest mb-36 ml2 font-bold" ref={textRef}>SKILLS</h2>
         <div className="w-full justify-center items-center">
           
           <div className="flex items-center mb-4">
-            <Image className=" mr-4 " src="/img/3d.png" alt="3d" width={68} height={68} />
-            <h3 className="text-2xl">3D</h3>
+            <Image className="mr-4" src="/img/3d.png" alt="3D" width={68} height={68} />
+            <h3 className="text-2xl px-4">3D</h3>
+
+            {object.metadata.skills.map(skill => (
+              <article className="mr-8 px-6" key={skill.model?.url || Math.random()}>
+                {skill.model?.url ? (
+                  <Image src={skill.model.url} alt="3D - logos" width={52} height={52} />
+                ) : (
+                  <p>Logo não disponível</p>
+                )}
+              </article>
+            ))}
           </div>
-        
-        
 
-
-
-        <div className=" w-full flex items-center">
-
-        <div className=" p-8 relative w-1 h-32 bg-transparent mb-4">
-          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full">
-            <div className="w-1 bg-white animate-drop"></div>
+          <div className="w-full flex items-center">
+            <div className="p-8 relative w-1 h-32 bg-transparent mb-4">
+              <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full">
+                <div className="w-1 bg-white animate-drop"></div>
+              </div>
+            </div>
           </div>
-        </div>
 
+          <div className="flex items-center mb-4">
+            <Image className="mr-4" src="/img/ui.png" alt="UI Designer" width={68} height={68} />
+            <h3 className="text-2xl px-4">UI Designer</h3>
 
-        {object.metadata.skills.map(skill => (
-          <article className="mr-8  px-6" key={skill.model?.url || Math.random()}>
-          {skill.model?.url ? (
-            <Image src={skill.model.url} alt="3d - logos" width={52} height={52}  />
-          ) : (
-            <p>Logo não disponível</p>
-          )}
-          </article>
-       ))}
-        </div>
-          
-        <div className="flex items-center mb-4">
-          <Image className=" mr-4 " src="/img/ui.png" alt="3d" width={68} height={68} />
-          <h3 className="text-2xl">UI Designer</h3>
-        </div>
-        
-
-        <div className="w-full flex items-center">
-
-        <div className=" p-8 relative w-1 h-32 bg-transparent mb-4">
-          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full">
-            <div className="w-1 bg-white animate-drop"></div>
+            {object.metadata.skill_logos.map(logo => (
+              <article className="mr-8 px-6" key={logo.ui?.url || Math.random()}>
+                {logo.ui?.url ? (
+                  <Image src={logo.ui.url} alt="UI - logos" width={52} height={52} />
+                ) : (
+                  <p>Logo não disponível</p>
+                )}
+              </article>
+            ))}
           </div>
-        </div>
 
+          <div className="w-full flex items-center">
+            <div className="p-8 relative w-1 h-32 bg-transparent mb-4">
+              <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full">
+                <div className="w-1 bg-white animate-drop"></div>
+              </div>
+            </div>
+          </div>
 
-        {object.metadata.skill_logos.map(logo => (
-          <article className="mr-8 mb-10 px-6" key={logo.ui?.url || Math.random()}>
-          {logo.ui?.url ? (
-            <Image src={logo.ui.url} alt="3d - logos" width={52} height={52}  />
-          ) : (
-            <p>Logo não disponível</p>
-          )}
-          </article>
-       ))}
-
-        </div>
-
-        <div className="flex items-center mb-4">
-        <Image className="mr-4" src="/img/dev.png" alt="3d" width={68} height={68} />
-        <h3 className="text-2xl">Front-End</h3>
-        </div>
-        
-
-
-        <div className="w-full flex items-center">
-        
-        <div className="  p-8 relative w-1 h-32 bg-transparent mb-4">
-          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full">
-           
+          <div className="flex items-center mb-4">
+            <Image className="mr-4" src="/img/dev.png" alt="Dev" width={68} height={68} />
+            <h3 className=" text-2xl px-4 ml-5">Dev</h3>
+            {object.metadata.skill_logos2.map(logo2 => (
+              <article className="mr-8 px-6" key={logo2.dev?.url || Math.random()}>
+                {logo2.dev?.url ? (
+                  <Image src={logo2.dev.url} alt="Dev - logos" width={52} height={52} />
+                ) : (
+                  <p>Logo não disponível</p>
+                )}
+              </article>
+            ))}
           </div>
         </div>
-
-
-
-        {object.metadata.skill_logos2.map(logo2 => (
-          <article className="mr-8 mb-10 px-6" key={logo2.dev?.url || Math.random()}>
-          {logo2.dev?.url ? (
-            <Image src={logo2.dev.url} alt="3d - logos" width={52} height={52}  />
-          ) : (
-            <p>Logo não disponível</p>
-          )}
-          </article>
-       ))}
-
-        </div>
-       
-        </div>
-        
-        
       </section>
     </div>
-  )
+  );
 }
